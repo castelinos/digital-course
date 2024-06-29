308d7a5807cd4f497bdc7c6b8b21570d512c4db1
castelinos <sheltcas10@gmail.com>
2024-06-29 20:11:44 +0530

    Removed landing page.Added protected route level

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,15 +4,30 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 interface ProtectedRouteProps {
+  level: 'admin'|'user';
   children: ReactNode;
 }
 
-const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const { user } = useAuth();
+const ADMIN_EMAIL = import.meta.env.VITE_ADMIN_EMAIL;
 
-  if(!user) {
-    /* changed to /login */
-    return <Navigate to="/login" replace />;
+const ProtectedRoute = ({ level, children }: ProtectedRouteProps) => {
+  const { user } = useAuth();
+  let searchParams;
+
+  switch (level) {
+    case "user":
+      if (!user) return <Navigate to="/login" replace />;
+      break;
+    case "admin":
+      searchParams = new URL(window.location.href).searchParams;
+      
+      if( searchParams.get('admin_email') !== ADMIN_EMAIL ){
+        return <Navigate to="/login" replace />;
+      }
+
+      break;
+    default:
+      return <Navigate to="/login" replace />;
   }
 
   return <>{children}</>;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App.tsx'
-import './index.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.tsx';
-import ProtectedRoute from './components/ProtectedRoute';
-import ProtectedComponent from './components/ProtectedRoute.tsx'; // Assume this is the component you want to protect
-import Login from './pages/Login.tsx'
-import Home from './pages/Home.tsx';
-import Register from './pages/Register.tsx';
-import { ResponsiveProvider } from './context/PlatformContext.tsx';
-import Lessons from './pages/Lessons.tsx';
-import BonusContent from './pages/BonusContent.tsx';
-
+import "./index.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/Login.tsx";
+import Home from "./pages/Home.tsx";
+import Register from "./pages/Register.tsx";
+import { ResponsiveProvider } from "./context/PlatformContext.tsx";
+import Lessons from "./pages/Lessons.tsx";
+import BonusContent from "./pages/BonusContent.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -20,33 +18,40 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ResponsiveProvider>
         <AuthProvider>
           <Routes>
-            {/* Added Route to redirect to register */}
-            <Route path="/" element={<Navigate to="/get-user" />} />
+            {/* Added Route to redirect root to login */}
+            <Route path="/" element={<Navigate to="/login" />} />
             {/* Added /login as login element route */}
             <Route path="/login" element={<Login />} />
             {/* Register User route */}
-            <Route path="/get-user" element={<Register />} />
+            <Route
+              path="/get-user"
+              element={
+                <ProtectedRoute level="admin">
+                  <Register />
+                </ProtectedRoute>
+              }
+            />
             <Route
               path="/home"
               element={
-                <ProtectedRoute>
-                  <ProtectedComponent children={<Home />} />
+                <ProtectedRoute level="user">
+                  <Home />
                 </ProtectedRoute>
               }
             />
             <Route
               path="/lessons"
               element={
-                <ProtectedRoute>
-                  <ProtectedComponent children={<Lessons />} />
+                <ProtectedRoute level="user">
+                  <Lessons />
                 </ProtectedRoute>
               }
             />
             <Route
               path="/bonus-content"
               element={
-                <ProtectedRoute>
-                  <ProtectedComponent children={<BonusContent />} />
+                <ProtectedRoute level="user">
+                  <BonusContent />
                 </ProtectedRoute>
               }
             />
