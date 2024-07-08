56750ebb75d7c9ef00aee9398c6a7e312aa0a001
castelinos <sheltcas10@gmail.com>
2024-07-08 22:31:46 +0530

    Formatted modal content for course promotion

--- src/pages/BonusContent.tsx
+++ src/pages/BonusContent.tsx
@@ -7,7 +7,6 @@ import { useRequireAuth } from "@/hooks/useRequireAuth";
 const BonusContent: React.FC = () => {
     useRequireAuth();
 
-
     return (
       <div
         style={{
@@ -31,13 +30,24 @@ const BonusContent: React.FC = () => {
                     justifyContent: "center",
                   }}
                 >
-                  <h1 style={{ textAlign: "center" }}>תוכן בונוס</h1>
-                  <p style={{ maxWidth: 400}}>
+                  <h1 style={{ textAlign: "center" }}>תוכן בונוס </h1>
+                  <p dir="auto" style={{ maxWidth: 400, textAlign: "justify" }}>
                     איך להתכתב עם נשים עד הפגישה , איך לבנות את הפרופיל המושלם ,
                     הורדה של כל קבצי הסטורים , ובונוסים של הרצאות והדרכות שלי על
                     מה לעשות בדייט
+                    <p style={{ color: "red", fontWeight: "bold" }}>
+                      שים לב בתשלום להשתמש באותו אמייל שרכשת את הקורס
+                    </p>
                   </p>
-                  <button>לחץ כאן To קנה עכשיו !</button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      window.location.href =
+                        import.meta.env.VITE_BONUS_CONTENT_PAY_URL;
+                    }}
+                  >
+                    קנה את זה עכשיו!
+                  </button>
                 </div>
               </div>
             </div>
