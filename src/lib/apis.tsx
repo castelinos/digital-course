4346733c8ce580c6615ad0c4d0e6dc2c8eb71483
castelinos <sheltcas10@gmail.com>
2024-07-08 22:27:56 +0530

    Added order api requests

--- src/lib/apis.tsx
+++ src/lib/apis.tsx
@@ -3,6 +3,15 @@ interface LoginAPIProps{
     password:string;
 }
 
+interface OrderProps {
+  email: string;
+  orderID: string;
+  customerID: string;
+  paymentID: string;
+  contact?: string;
+  name?: string;
+}
+
 export async function getCustomerData( customerID: string ) {
     try {
         const response = await fetch(
@@ -38,21 +47,20 @@ export async function getCustomerData( customerID: string ) {
 
 export async function loginUser({ username, password }:LoginAPIProps) {
    try {
-        const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, password }),
-        });
+      const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/login`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (response.ok) {
-        return ({status:"success", message:data.message });
-    }
-    else{
-        console.log(data.message);
-        throw new Error(data.message || "An error occurred");
-    }
+      if (response.ok) {
+          return ({status:"success", message:data.message });
+      }
+      else{
+          throw new Error(data.message || "An error occurred");
+      }
     
    // eslint-disable-next-line @typescript-eslint/no-explicit-any
    } catch ( error:any ) {
@@ -87,4 +95,63 @@ export async function registerUser(username: string, password: string) {
         console.error("Error during registration:", error);
         return({status:"error", message: error.message})
     }
+}
+
+export async function createOrder({email,orderID,customerID,paymentID,contact,name}:OrderProps) {
+  try {
+    const body = {
+      email,
+      order_id: orderID,
+      customer_id: customerID,
+      payment_id: paymentID,
+    };
+     
+    if( contact && contact.trim() ) Object.assign(body, {contact});
+
+    if (name && name.trim()) Object.assign(body, { name });
+    
+    const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/orders`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return { status: "success", message: data.message };
+    } else {
+      throw new Error(data.message);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error("Error during registration:", error);
+    return { status: "error", message: error.message };
+  }
+}
+
+export async function getUserOrders( email: string ) {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_APP_API_URL}/orders/?username=${email}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return { status: "success", data: data };
+    } else {
+      throw new Error(data.message || "An error occurred");
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return { status: "error", message: error.message };
+  }
 }
