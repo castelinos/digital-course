b914e138b3bfb8a8f77ff6b15c9effe5a28d16a5
castelinos <sheltcas10@gmail.com>
2024-11-24 00:11:15 +0530

    Created reusable modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,16 +6,24 @@ interface ModalProps {
     children?: ReactNode,
     showModal: boolean,
     toggleShowModal: Function,
+    backgroundTheme: string,
 }
 
-const Modal: React.FC<ModalProps> = ({ children, showModal, toggleShowModal }) => {
+const Modal: React.FC<ModalProps> = ({ children, showModal, toggleShowModal, backgroundTheme }) => {
 
-    const visible = showModal ? "visible":"hidden"
+    const visible = showModal ? "visible":"hidden";
+    let backgroundHexCode = "#ffffff";
+    let fontColor = "#000000";
+
+    if( backgroundTheme === "dark") {
+      backgroundHexCode = "#000000";
+      fontColor = "#ffffff"
+    }
 
     return (
       <div
         style={{
-            zIndex:"3",
+            zIndex:"10",
             position:"fixed",
             inset:0,
             width: "100vw",
@@ -23,20 +31,34 @@ const Modal: React.FC<ModalProps> = ({ children, showModal, toggleShowModal }) =
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            backgroundColor:"rgba(0, 0, 0, 0.7)",
             visibility: visible
         }}
       >
-        <div className="card">
-            <div className="card-body"
-                style={{
-                display: "flex",
-                flexDirection: "column",
-                alignContent: "center",
-                justifyContent: "center"
-                }}
-            >
-              { children }
-            </div>
+        <div className="card" style={{maxWidth:"600px",backgroundColor: backgroundHexCode, padding:"0"}}>
+            <div style={{ 
+              zIndex:3,
+              position:"absolute",
+              top:10,
+              right:10,
+              cursor:"pointer", 
+              margin:"15px 15px 0 0", 
+              backgroundColor:"transparent",
+              color:fontColor 
+              }} 
+              onClick={()=>{ toggleShowModal(false) 
+            }}>&#9932;</div>
+
+          <div className="card-body"
+            style={{
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "center",
+            justifyContent: "center"
+            }}
+          >
+            { children }
+          </div>
         </div>
       </div>
     );
